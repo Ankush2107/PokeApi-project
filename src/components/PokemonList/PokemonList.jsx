f976deb3349a07d2d86bc40f4daae7622c59fb01
Ankush2107 <ankush.kumar210701@gmail.com>
2025-07-11 10:42:23 +0530

    added css file

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import "./PokemonList.css";
 function PokemonList() {
     const [ allPokemons, setAllPokemons ] = useState([]);
     const [ searchTerm, setSearchTerm ] = useState("");
@@ -18,9 +19,13 @@ function PokemonList() {
                     return {
                         id: pokemonData.id,
                         name: pokemonData.name,
-                        imageUrl: pokemonData.sprites.other.dream_world.front_default
+                        imageUrl: pokemonData.sprites.other.dream_world.front_default,
+                        height: pokemonData.height,
+                        weight: pokemonData.weight,
+                        type: pokemonData.types.filter(ele => ele.type.name)
                     }
                 })
+                console.log(pokemonWithDetails)
                 setAllPokemons(pokemonWithDetails)
                 setLoading(false)
             } catch (error) {
@@ -34,7 +39,7 @@ function PokemonList() {
     const filteredPokemons = allPokemons.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
     console.log(filteredPokemons)
-    
+
     const handleSearchChange = (event) => {
         setSearchTerm(event.target.value);
         if(loading){
@@ -44,10 +49,17 @@ function PokemonList() {
   return (
     <div className="pokemon-list-container">
       <input type="text" placeholder="Search pokemon by name..." value={searchTerm} onChange={handleSearchChange} className="search-bar" />
+
       <div className="pokemon-grid">
         {filteredPokemons.map(pokemon => (
             <Link to={`/pokemon/${pokemon.name}`} key={pokemon.name} className="pokemon-card" >
-                <p>{pokemon.name.charAt(0).toUpperCase() +  pokemon.name.slice}</p>
+                <img src={pokemon.imageUrl} alt={pokemon.name} />
+                {pokemon.type.map((ele) => (
+                    <p>{ele.name}</p>
+                ) )}
+                <p>{pokemon.name.charAt(0).toUpperCase() +  pokemon.name.slice(1)}</p>
+                <p>height: {pokemon.height}</p>
+                <p>weight: {pokemon.weight}</p>
             </Link>
         ))}
       </div>
